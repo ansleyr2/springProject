5dd46245eb763b4ceb67cdf547c980ca28b5b48d
Ansley Rodrigues <ansleyr2@gmail.com>
2018-11-07 20:19:42 -0500

    Added put and patch end points

--- src/main/java/com/ansrod/vocab/business/domain/LearnedWord.java
+++ src/main/java/com/ansrod/vocab/business/domain/LearnedWord.java
@@ -2,10 +2,26 @@ package com.ansrod.vocab.business.domain;
 
 import java.util.Date;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class LearnedWord {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
 	private long wordId;
 	private String word;
 	private String meaning;
+	public LearnedWord(String word, String meaning, String sentence) {
+		//super();
+		this.word = word;
+		this.meaning = meaning;
+		this.sentence = sentence;
+	}
+	public LearnedWord() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	private String sentence;
 	private Date date;
 	
